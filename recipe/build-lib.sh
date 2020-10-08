381c236cc156c3026394892f229ab5ee183fcf7e
H. Vetinari <h.vetinari@gmx.com>
2020-10-08 09:47:08 +0200

    Start building for Ampere on Cuda 11 and future-proof script
    Suggested-By: Thejaswi Rao <rao.thejaswi@gmail.com>
    Coauthored-By: Thejaswi Rao <rao.thejaswi@gmail.com>

--- recipe/build-lib.sh
+++ recipe/build-lib.sh
@@ -1,3 +1,6 @@
+# function for facilitate version comparison; cf. https://stackoverflow.com/a/37939589
+function version2int { echo "$@" | awk -F. '{ printf("%d%02d\n", $1, $2); }'; }
+
 CUDA_CONFIG_ARG=""
 if [ ${cuda_compiler_version} != "None" ]; then
     # for documentation see e.g.
@@ -8,10 +11,15 @@ if [ ${cuda_compiler_version} != "None" ]; then
     # the following are all the x86-relevant gpu arches; for building aarch64-packages, add: 53, 62, 72
     ARCHES=(35 50 52 60 61 70)
     LATEST_ARCH=70
-    if [ ${cuda_compiler_version} != "9.2" ]; then
-        # cuda 9.2 does not support Turing (sm_75)
+    if [ $(version2int $cuda_compiler_version) -ge $(version2int "10.0") ]; then
+        # cuda >= 10.0 supports Turing (sm_75)
         ARCHES+=(75)
         LATEST_ARCH=75
+    fi
+    if [ $(version2int $cuda_compiler_version) -ge $(version2int "11.0") ]; then
+        # cuda >= 11.0 supports Ampere (sm_80)
+        ARCHES+=(80)
+        LATEST_ARCH=80
     fi
     for arch in "${ARCHES[@]}"; do
         CUDA_ARCH="${CUDA_ARCH} -gencode=arch=compute_${arch},code=sm_${arch}";
