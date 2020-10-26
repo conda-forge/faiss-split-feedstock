b3ae8a72f212999afbac07f1666a0d18648344a1
H. Vetinari <h.vetinari@gmx.com>
2020-10-26 23:57:43 +0100

    update unix scripts to new build infra

--- recipe/build-lib.sh
+++ recipe/build-lib.sh
@@ -1,6 +1,8 @@
 # function for facilitate version comparison; cf. https://stackoverflow.com/a/37939589
 function version2int { echo "$@" | awk -F. '{ printf("%d%02d\n", $1, $2); }'; }
 
+set -e
+
 CUDA_CONFIG_ARG=""
 if [ ${cuda_compiler_version} != "None" ]; then
     # for documentation see e.g.
@@ -22,24 +24,28 @@ if [ ${cuda_compiler_version} != "None" ]; then
         LATEST_ARCH=80
     fi
     for arch in "${ARCHES[@]}"; do
-        CUDA_ARCH="${CUDA_ARCH} -gencode=arch=compute_${arch},code=sm_${arch}";
+        CUDA_ARCHS="${CUDA_ARCHS} ${arch}-virtual";
     done
     # to support PTX JIT compilation; see first link above or cf.
     # devblogs.nvidia.com/cuda-pro-tip-understand-fat-binaries-jit-caching
-    CUDA_ARCH="${CUDA_ARCH} -gencode=arch=compute_${LATEST_ARCH},code=compute_${LATEST_ARCH}"
+    # see also cmake.org/cmake/help/latest/prop_tgt/CUDA_ARCHITECTURES.html
+    CUDA_ARCHS="${CUDA_ARCHS} ${LATEST_ARCH}-real"
 
-    CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
+    FAISS_ENABLE_GPU="ON"
+    CUDA_CONFIG_ARG="-DCMAKE_CUDA_ARCHITECTURES=\"${CUDA_ARCHS}\" "
 else
-    CUDA_CONFIG_ARG="--without-cuda"
+    FAISS_ENABLE_GPU="OFF"
 fi
 
 # Build vanilla version (no avx)
-./configure --prefix=${PREFIX} --exec-prefix=${PREFIX} \
-  --with-blas=-lblas --with-lapack=-llapack \
-  ${CUDA_CONFIG_ARG} --with-cuda-arch="${CUDA_ARCH}" || exit 1
+cmake -B _build_generic \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_TESTING=ON \
+      -DFAISS_ENABLE_PYTHON=OFF \
+      -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+      -DCMAKE_BUILD_TYPE=Release \
+      ${CUDA_CONFIG_ARG} \
+      .
 
-# make sets SHAREDEXT correctly for linux/osx
-make install
-
-# make builds libfaiss.a & libfaiss.so; we only want the latter
-rm ${PREFIX}/lib/libfaiss.a
+cmake --build _build_generic -j $CPU_COUNT
+cmake --install _build_generic --prefix $PREFIX
--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -1,15 +1,23 @@
-CUDA_CONFIG_ARG=""
+set -e
+
+FAISS_ENABLE_GPU=""
 if [ ${cuda_compiler_version} != "None" ]; then
-    CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
+    FAISS_ENABLE_GPU="ON"
 else
-    CUDA_CONFIG_ARG="--without-cuda"
+    FAISS_ENABLE_GPU="OFF"
 fi
 
-# Build vanilla version (no avx)
-./configure --with-blas=-lblas --with-lapack=-llapack ${CUDA_CONFIG_ARG}
+# Build vanilla version (no avx2), see build-lib.sh
+# Do not use the Python3_* variants for cmake
+cmake -B _build_python \
+      -Dfaiss_ROOT=${PREFIX}\
+      -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DPython_EXECUTABLE="${PYTHON}" \
+      faiss/python
 
-make -C python build
+cmake --build _build_python -j $CPU_COUNT
 
-cd python
-
-$PYTHON -m pip install . -vv
+# Build actual python module.
+cd _build_python
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt --prefix=$PREFIX
