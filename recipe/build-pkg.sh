3a10dcaac1e82111b0a1182f5827f81cfd123f18
H. Vetinari <h.vetinari@gmx.com>
2020-12-08 08:48:42 +0100

    add nvcc-workaround also to build-pkg.sh

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -3,6 +3,15 @@ set -e
 FAISS_ENABLE_GPU=""
 if [ ${cuda_compiler_version} != "None" ]; then
     FAISS_ENABLE_GPU="ON"
+    # Acc. to https://cmake.org/cmake/help/v3.19/module/FindCUDAToolkit.html#search-behavior
+    # CUDA toolkit is search relative to `nvcc` first before considering
+    # "-DCUDAToolkit_ROOT=${CUDA_HOME}". We have multiple workarounds:
+    #   - Add symlinks from ${CUDA_HOME} to ${BUILD_PREFIX}
+    #   - Add ${CUDA_HOME}/bin to ${PATH}
+    #   - Remove `nvcc` wrapper in ${BUILD_PREFIX} so that `nvcc` from ${CUDA_HOME} gets found.
+    # TODO: Fix this in nvcc-feedstock or cmake-feedstock.
+    # NOTE: It's okay for us to not use the wrapper since CMake adds -ccbin itself.
+    rm "${BUILD_PREFIX}/bin/nvcc"
 else
     FAISS_ENABLE_GPU="OFF"
 fi
