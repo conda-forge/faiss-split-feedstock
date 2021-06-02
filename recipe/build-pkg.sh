1514c3a189a71fbcc997c5c33c60b38c93c502a1
H. Vetinari <h.vetinari@gmx.com>
2021-06-02 22:48:01 +0200

    reflect upstream build changes for separating libfaiss/libfaiss_avx2

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -16,6 +16,12 @@ else
     FAISS_ENABLE_GPU="OFF"
 fi
 
+if [[ $CF_FAISS_BUILD == avx2 ]]; then
+    TARGET="swigfaiss_avx2"
+else
+    TARGET="swigfaiss_avx2"
+fi
+
 # Build vanilla version (no avx2), see build-lib.sh
 cmake -B _build_python_generic \
       -Dfaiss_ROOT=_libfaiss_generic_stage/ \
@@ -23,7 +29,7 @@ cmake -B _build_python_generic \
       -DCMAKE_BUILD_TYPE=Release \
       -DPython_EXECUTABLE="${PYTHON}" \
       faiss/python
-cmake --build _build_python_generic -j $CPU_COUNT
+cmake --build _build_python_generic --target ${TARGET} -j $CPU_COUNT
 
 # Build version with avx2 support, see build-lib.sh
 cmake -B _build_python_avx2 \
@@ -33,11 +39,11 @@ cmake -B _build_python_avx2 \
       -DCMAKE_BUILD_TYPE=Release \
       -DPython_EXECUTABLE="${PYTHON}" \
       faiss/python
-cmake --build _build_python_avx2 -j $CPU_COUNT
+cmake --build _build_python_avx2 --target ${TARGET} -j $CPU_COUNT
 
 # copy generated swig module with avx2-support to specifically named file, cf.
-# https://github.com/facebookresearch/faiss/blob/v1.7.0/faiss/python/setup.py#L25-L26
-cp _build_python_avx2/swigfaiss.py _build_python_generic/swigfaiss_avx2.py
+# https://github.com/facebookresearch/faiss/blob/v1.7.1/faiss/python/setup.py#L37-L40
+cp _build_python_avx2/swigfaiss_avx2.py _build_python_generic/swigfaiss_avx2.py
 cp _build_python_avx2/_swigfaiss_avx2.so _build_python_generic/_swigfaiss_avx2.so
 
 # Build actual python module.
