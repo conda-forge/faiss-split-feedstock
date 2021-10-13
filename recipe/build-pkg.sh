fba859fed4292d7a84060132cbc23b9252c3c7ff
H. Vetinari <h.vetinari@gmx.com>
2021-10-13 21:42:48 +1100

    don't build and link avx2 lib for osx-arm

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -25,20 +25,23 @@ cmake -B _build_python_generic \
       faiss/python
 cmake --build _build_python_generic --target swigfaiss -j $CPU_COUNT
 
-# Build version with avx2 support, see build-lib.sh
-cmake -B _build_python_avx2 \
-      -Dfaiss_ROOT=_libfaiss_avx2_stage/ \
-      -DFAISS_OPT_LEVEL=avx2 \
-      -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DPython_EXECUTABLE="${PYTHON}" \
-      faiss/python
-cmake --build _build_python_avx2 --target swigfaiss_avx2 -j $CPU_COUNT
+if [[ "${target_platform}" != osx-arm64 ]]; then
+  # Build version with avx2 support, see build-lib.sh;
+  # osx-arm is not an x86-64 architecture and never has AVX2 instructions
+  cmake -B _build_python_avx2 \
+        -Dfaiss_ROOT=_libfaiss_avx2_stage/ \
+        -DFAISS_OPT_LEVEL=avx2 \
+        -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DPython_EXECUTABLE="${PYTHON}" \
+        faiss/python
+  cmake --build _build_python_avx2 --target swigfaiss_avx2 -j $CPU_COUNT
 
-# copy generated swig module with avx2-support to specifically named file, cf.
-# https://github.com/facebookresearch/faiss/blob/v1.7.1/faiss/python/setup.py#L37-L40
-cp _build_python_avx2/swigfaiss_avx2.py _build_python_generic/swigfaiss_avx2.py
-cp _build_python_avx2/_swigfaiss_avx2.so _build_python_generic/_swigfaiss_avx2.so
+  # copy generated swig module with avx2-support to specifically named file, cf.
+  # https://github.com/facebookresearch/faiss/blob/v1.7.1/faiss/python/setup.py#L37-L40
+  cp _build_python_avx2/swigfaiss_avx2.py _build_python_generic/swigfaiss_avx2.py
+  cp _build_python_avx2/_swigfaiss_avx2.so _build_python_generic/_swigfaiss_avx2.so
+fi
 
 # Build actual python module.
 pushd _build_python_generic
@@ -46,4 +49,4 @@ $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 popd
 # clean up cmake-cache between builds
 rm -r _build_python_generic
-rm -r _build_python_avx2
+rm -r _build_python_avx2 || true
