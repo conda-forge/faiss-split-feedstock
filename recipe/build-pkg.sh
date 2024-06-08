2b6943b666fba50480a4ce9d6f91c770f28942d3
H. Vetinari <h.vetinari@gmx.com>
2024-06-08 14:12:03 +1100

    help CMake find right python in cross-compilation

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -13,13 +13,19 @@ if [[ "${target_platform}" == linux-* ]]; then
     export CXXFLAGS="$CXXFLAGS -DSWIGWORDSIZE64"
 fi
 
+PYTHON_LIB="${PREFIX}/lib/libpython${PY_VER}${SHLIB_EXT}"
+PYTHON_INC="$PREFIX/include/python${PY_VER}"
+NUMPY_INC=$(python -c 'import numpy;print(numpy.get_include())')
+
 # Build vanilla version (no avx2), see build-lib.sh
 cmake -G Ninja \
     ${CMAKE_ARGS} \
     -Dfaiss_ROOT=_libfaiss_generic_stage/ \
     -DFAISS_ENABLE_GPU=${FAISS_ENABLE_GPU} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DPython_EXECUTABLE="${PYTHON}" \
+    -DPython_INCLUDE_DIRS="${PYTHON_INC}" \
+    -DPython_LIBRARIES="${PYTHON_LIB}" \
+    -DPython_NumPy_INCLUDE_DIRS=${NUMPY_INC} \
     -B _build_python_generic \
     faiss/python
 cmake --build _build_python_generic --target swigfaiss -j $CPU_COUNT
