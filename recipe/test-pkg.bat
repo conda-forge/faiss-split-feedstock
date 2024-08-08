506cbd133b5937f82e145fd6698759540084ae64
H. Vetinari <h.vetinari@gmx.com>
2024-08-08 16:00:13 +1100

    be less spammy with test suite logs

--- recipe/test-pkg.bat
+++ recipe/test-pkg.bat
@@ -3,7 +3,7 @@
 python -c "from numpy.core._multiarray_umath import __cpu_features__; print(f'Testing version with AVX2-support - ' + str(__cpu_features__['AVX2']))"
 pytest tests --log-file-level=INFO --log-file=log.txt -k "not %SKIPS%" --durations=50
 if %ERRORLEVEL% neq 0 exit 1
-type log.txt
+:: type log.txt
 
 :: this should have run without AVX2
 python -c "q = open('log.txt').readlines(); import sys; sys.exit(0 if 'Successfully loaded faiss.' in [x[35:-1] for x in q] else 1)"
